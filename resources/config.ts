e4b27537245165a431df19d80dab2785f97a8b77
lucasvtiradentes <lucasvtiradentes@gmail.com>
2023-04-16 00:12:46 -0300

    :sparkles: feature: add option to specify team per game (#25)

--- resources/config.ts
+++ resources/config.ts
@@ -1,16 +1,40 @@
 // prettier-ignore
 export const config = {
   esports: {
-    favoriteTeams: ['loud', 'mibr', 'imperial'], // specify your favorite teams
-    games: {                                     // select the games you're interested
-      csgo: true,
-      valorant: true,
-      rainbowSixSiege: true,
-      leagueOfLegends: true,
-      overwatch: true,
-      rocketLeague: true,
-      dota: true,
-      callOfDuty: true
+    favoriteTeams: ['mibr'],              // specify your global favorite teams, that will be search in all games
+    games: {                              // select the games you're interested
+      csgo: {
+        sync: true,
+        teams: ['imperial']               // specify the teams you want to search only in this game
+      },
+      valorant: {
+        sync: true,
+        teams: ['sentinels', 'furia', 'loud', 'mibr']
+      },
+      rainbowSixSiege: {
+        sync: true,
+        teams: ['nip', 'faze', 'liquid', 'w7m']
+      },
+      leagueOfLegends: {
+        sync: true,
+        teams: []
+      },
+      overwatch: {
+        sync: true,
+        teams: []
+      },
+      rocketLeague: {
+        sync: true,
+        teams: []
+      },
+      dota: {
+        sync: true,
+        teams: []
+      },
+      callOfDuty: {
+        sync: true,
+        teams: []
+      }
     }
   },
   datetime: {
@@ -19,8 +43,8 @@ export const config = {
   },
   settings: {
     notifyOnlyAboutTodayGames: true,             // if 'false' it will alse send email in case of matchs of favorite teams in the next days
-    strictTeamComparasion: false,                // if 'true' the name of the teams must be exact in all the matches source sites
-    maintanceMode: false,                        // development option dont need to change
-    loopFunction: 'checkTodayGames'              // development option dont need to change
+    strictTeamComparasion: true,                 // if 'true' the name of the teams must be exact in all the matches source sites
+    maintanceMode: false,                        // development option, dont need to change
+    loopFunction: 'checkTodayGames'              // development option, dont need to change
   }
 };
